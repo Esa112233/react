3f056a2e7c7526c9d24fdd4faa75c7900a9a20fb
Esa112233 <57723779+Esa112233@users.noreply.github.com>
2023-09-07 08:34:04 +0000

    do colours and then ur done

--- portfolio-next/app/page.js
+++ portfolio-next/app/page.js
@@ -18,17 +18,16 @@ export default function Home() {
           <nav className=' py-10 mb-12 flex justify-between'>
             <h1 className='text-xl font-burtons text dark:text-pink-400'>Esa Abuzar</h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill  onClick={() => setDarkMode(!darkMode)} className=' cursor-pointer text-2xl dark:text-white'/></li>
-              <li><a className=' bg-gradient-to-r from-pink-500 to-fuchsia-300 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
-              </li>
+              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=' cursor-pointer text-2xl dark:text-white'/></li>
+        
             </ul>
           </nav>
           <div className=' text-center p-10'>
-            <h2 className=' text-7xl text-pink-400 font-large'>Hello, I'm Esa </h2>
+            <h2 className={darkMode ? ' text-7xl text-pink-400 font-large' : 'text-7xl text-teal-600 font-large'}>Hello, I'm Esa </h2>
             <h3 className=' text-2xl py-2 dark:text-white'>Computer Engineering Co-op Student</h3>
-            <p className=' text-md py-5 leading-8 text-gray-800 dark:text-white'>gyfugwdyifgherihgfergf8yerhgfywhg</p>
+            <p className=' text-md py-5 leading-8 text-gray-800 dark:text-white'>Check out my <a className={darkMode ? ' bg-gradient-to-r from-pink-500 to-fuchsia-300 bg-cyan-500 text-white px-4 py-2 rounded-md' : 'bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md'} href='#'>Resume!</a></p>
           </div>
-          <div className=' text-5xl flex justify-center gap-16 py-3 dark:text-pink-400'>
+          <div className=' text-5xl flex justify-center gap-16 py-3 dark:text-pink-400 text-teal-600'>
             <a target='_blanck' href='https://www.linkedin.com/in/esa-abuzar/'>
               <AiFillLinkedin/>
               </a>
@@ -56,9 +55,9 @@ export default function Home() {
           <section>
             
         <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-pink-500" }>
+          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-gradient-to-b from-blue-500 to-white text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1" }>
             
-              <h3  className=' text-3xl text-white'>Papa Web Scraper</h3>
+              <h3  className=' text-3xl text-white'><a href='https://github.com/Esa112233/Django-Chat-App' target='_blanck'>Papa Web Scraper</a></h3> <p className=' text-black'>PyQt5 • Selenium • MySQL • Multithreading</p>
               <div class="list-disc text-white pt-4">
   <p>-Developed a web scraper application that extracts emails and links from all Google pages of a given URL through a graphical User Interface (GUI) using PyQt5 for the front-end </p>
   <br/>
@@ -71,8 +70,8 @@ export default function Home() {
         </div>
 
 
-          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-pink-500" }>
-          <h3  className=' text-3xl text-white'>QuickChat</h3>
+          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : "bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 " }>
+          <h3  className=' text-3xl text-white'><a href='https://github.com/Esa112233/Django-Chat-App' target='_blanck' >QuickChat</a></h3> <p className=' text-black'>Django • Django Channels • WebSockets • HTML•  Bootstrap • JavaScript</p>
           <div class="list-disc text-white pt-4">
             <p>-Created a real-time chat application powered by Django and WebSockets, allowing users to join dynamic chat rooms and connect with people online</p>
             <br/>
@@ -89,10 +88,11 @@ export default function Home() {
             <h3 className='text-3xl py-1 text-black dark:text-white mt-20'>Work Experience</h3>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-pink-500" }>
+          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-gradient-to-b from-teal-500 to-white text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1" }>
             
-              <h3  className=' text-3xl text-white'>Papa Sports</h3>
-              <div class="list-disc text-white pt-4">
+              <h3  className=' text-3xl text-black dark:text-white '>Papa Sports</h3>
+              <br/> <h2 className='text-2xl dark:text-black text-white'>Marketing Agent</h2>
+              <div class="list-disc dark:text-white text-black pt-4">
   <p>-Contacted over 400 potential customers for boxing apparel and equipment, resulting in increased product inquiries </p>
   <br/>
   <p>-Trained a new employee on marketing procedures and YAMM software to send emails</p>
@@ -102,9 +102,10 @@ export default function Home() {
         </div>
 
 
-          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-pink-500" }>
-          <h3  className=' text-3xl text-white'>Sales Associate</h3>
-          <div class="list-disc text-white pt-4">
+          <div className={darkMode ? ' bg-gradient-to-b from-pink-500 to-gray-900 text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1 ' : " bg-gradient-to-b  from-teal-500 to-white text-center p-10 rounded-lg divide-y-20 shrink basis-1/3 flex-1" }>
+          <h3  className=' text-3xl dark:text-white text-black'>COBS Bread</h3>
+          <br/> <h2 className='text-2xl  dark:text-black text-white'>Sales Associate</h2>
+          <div class="list-disc dark:text-white text-black pt-4">
             <p>-Advised customers on product selection to meet dietary restrictions and needs</p>
             <br/>
             <p>-Collaborated with over 6 team members to serve customers during rush hours</p>
