6bf84e0effb1a2e614e408d7e8e96fc5a2fa69e6
Esa112233 <57723779+Esa112233@users.noreply.github.com>
2023-09-12 05:50:47 +0000

    update colour

--- portfolio-next/app/page.js
+++ portfolio-next/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       <main className=' bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
         <section className='min-h-screen'>
           <nav className=' py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons text dark:text-pink-400'>Esa Abuzar</h1>
+            <h1 className='text-xl font-burtons text dark:text-pink-400 text-teal-600'>Esa Abuzar</h1>
             <ul className='flex items-center'>
               <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=' cursor-pointer text-2xl dark:text-white'/></li>
         
